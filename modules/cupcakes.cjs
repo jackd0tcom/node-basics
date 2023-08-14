e4f8fa4983b38032246e796545603d7039eeb7d5
Jack Ball <ball.jack2376@gmail.com>
2023-08-14 12:33:12 -0600

    common js

---
+++ modules/cupcakes.cjs
@@ -0,0 +1,20 @@
+const { prettyPrintCupcake } = require("./cupcake-printer.cjs");
+
+const cupcakes = [
+  {
+    flavor: "vanilla",
+    price: 2.0,
+  },
+  {
+    flavor: "chocolate",
+    price: 2.5,
+  },
+  {
+    flavor: "red velvet",
+    price: 3.0,
+  },
+];
+
+for (const cupcake of cupcakes) {
+  console.log(prettyPrintCupcake(cupcake));
+}
